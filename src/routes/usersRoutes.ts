787c97149003d368c28a7b7d33f20d2df3cbf62e
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2025-03-26 15:29:04 +0100

    added middleware + register and login routes

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,11 +1,32 @@
 
 import express from "express";
+
 import jwt from 'jsonwebtoken';
 import {app, prisma} from "../app";
 
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = 'secret';
+import bcrypt from 'bcryptjs';
 
+declare module 'express' {
+    interface Request {
+        user?: any;
+    }
+}
+
+//Middleware
+const authenticationMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).send('Unauthorized');
+    jwt.verify(token, JWT_SECRET, (err,user) => {
+        if (err) return res.status(403).send('Forbidden');
+        req.user = user;
+        next();
+    });
+};
+
+
+//Connexion
 app.post('/api/auth/login', async (req, res) => {
     const { email, password } = req.body;
     const user = await prisma.user.findUnique({ where: { email } });
@@ -15,6 +36,20 @@ app.post('/api/auth/login', async (req, res) => {
     } else {
         res.status(401).send('Invalid credentials');
     }
+});
+
+//Register
+app.post('/api/auth/register', async (req, res) => {
+    const {fullname, email, password} = req.body;
+
+    if(!fullname || !email || !password){
+        res.status(400).json({error: 'Please fill all the fields'});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({ data: { fullname, email, password } });
+
+    res.status(201).json({message: 'User created successfully'});
 });
 
 module.exports = router;
