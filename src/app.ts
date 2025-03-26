555db1a5fc4f8d24752360bb2561bbf2a81c3c0c
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2025-03-26 12:15:53 +0100

    initialize back

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import {PrismaClient} from '@prisma/client';
+
+const app = express();
+const port = 3000;
+
+const prisma = new PrismaClient()
+
+
+
+app.get('/', async (req: Request, res: Response) => {
+    const result = await prisma.test.findMany()
+    res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
